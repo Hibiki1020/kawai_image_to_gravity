989320713fd64ad539573921f500df26729cf76d
Hibiki Kawai <hibikijitaku@gmail.com>
2021-08-08 12:19:37 +0900

    change yaml

--- mle/original_dataset.py
+++ mle/original_dataset.py
@@ -19,6 +19,7 @@ class OriginalDataset(data.Dataset):
     def __getitem__(self, index):
         img_path = self.data_list[index][0]
         img = Image.open(img_path)
+        img = img.convert("RGB")
 
         acc_str_list = self.data_list[index][6:]
         acc_list = [float(num) for num in acc_str_list]
